ac89bd9ba81113e3ed268efdef4e61ae8cc75a86
valdesius <v.inc@inbox.ru>
2024-07-13 22:30:12 +0300

    v0.01

---
+++ src/main/java/com/valdesius/crossedpathsstories/CrossedPathsStoriesApplication.java
@@ -0,0 +1,13 @@
+package com.valdesius.crossedpathsstories;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrossedPathsStoriesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrossedPathsStoriesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/valdesius/crossedpathsstories/CrossedPathsStoriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valdesius.crossedpathsstories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrossedPathsStoriesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
